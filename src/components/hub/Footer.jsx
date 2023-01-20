147522750b38372a6491df6f848a5f8f909870f2
Aaron <mol.aron@hotmail.com>
2023-01-20 02:12:02 +0100

    v.4.5 - Added Social Media link and usage document

--- src/components/hub/Footer.jsx
+++ src/components/hub/Footer.jsx
@@ -8,46 +8,46 @@ const Footer = () => {
     const {loggedIn} = useContext(LoggedInContext);
 
     return (
-        <div className="footer section__padding">
+        <div className = "footer section__padding">
 
-            <div className="footer-heading">
-                <h1 className="gradient__text"> Szeretne mások előtt belépni a jövőbe? </h1>
+            <div className = "footer-heading">
+                <h1 className = "gradient__text"> Szeretne mások előtt belépni a jövőbe? </h1>
             </div>
 
 
-            <div className="footer-button">
+            <div className = "footer-button">
                 {loggedIn ?
                     null
                     :
-                    <Link to="/authentication"> Belépek </Link>
+                    <Link to = "/authentication"> Belépek </Link>
                 }
             </div>
 
 
-            <div className="footer-links">
-                <div className="footer-links_logo">
+            <div className = "footer-links">
+                <div className = "footer-links_logo">
                     <h1> MovieSeeker </h1>
                     <p> Nyíregyháza 4400 <br/> Szabolcs-Szatmár-Bereg </p>
-                    <p className="rights"> Minden jog fenntartva. </p>
+                    <p className = "rights"> Minden jog fenntartva. </p>
                 </div>
 
 
-                <div className="footer-links_div">
+                <div className = "footer-links_div">
                     <h4> Hivatkozások </h4>
-                    <p> Közösségi média </p>
-                    <p><Link to="/about"> Rólunk </Link></p>
+                    <p> <a target = "blank" href = "https://twitter.com/MovieSeek3r"> Közösségi média </a> </p>
+                    <p><Link to = "/about"> Rólunk </Link></p>
                 </div>
 
 
-                <div className="footer-links_div">
+                <div className = "footer-links_div">
                     <h4> Cégünk </h4>
-                    <p> Adatkezelési tájékozatató </p>
-                    <p> Felhasználási feltételek </p>
-                    <p> Szerzői jogok </p>
+                    <p> <a target = "blank" href="https://drive.google.com/file/d/1O7P17BDGbDbYa2bs5kAxzs7Qs4mNYNw2/view?usp=share_link"> Adatkezelési tájékozatató </a> </p>
+                    <p> <a target = "blank" href="https://drive.google.com/file/d/1gE48SSNeFcSadBxaXZ76j_22z7hf2CLE/view?usp=share_link"> Felhasználási feltételek </a> </p>
+                    <p> <a target = "blank" href="https://drive.google.com/file/d/1--hf6fmrVw2x95QdJZyQ8BjVreU0oCHH/view?usp=share_link"> Szerzői jogok </a> </p>
                 </div>
 
 
-                <div className="footer-links_div">
+                <div className = "footer-links_div">
                     <h4> Lépjen kapcsolatba </h4>
                     <p> Nyíregyháza 4400 <br/> Szabolcs-Szatmár-Bereg </p>
                     <p> 085-132567 </p>
@@ -56,7 +56,7 @@ const Footer = () => {
             </div>
 
 
-            <div className="footer-copyright">
+            <div className = "footer-copyright">
                 <p> @2023 MovieSeeker. Minden jog fenntartva. </p>
             </div>
 
