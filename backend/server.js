85edfa1a8547aabd7b040be2fa61dbc34990f6e5
Legen-daaary <fut.tibor@gmail.com>
2023-02-05 17:08:10 +0100

    Remove Sequelieze force from server.js

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,11 @@ import apiRouter from "./src/routes/api.route.js";
 import authorizationRouter from "./src/routes/authorization.route.js";
 import authenticationRouter from "./src/routes/authentication.route.js";
 import commentsRouter from "./src/routes/comments.route.js";
+import ratingRouter from "./src/routes/rating.route.js";
 import {PORT} from "./src/configs/server.config.js";
 import associationsInitializer from "./src/utils/assocationsInitalizer.util.js";
-import getConnectionService from "./src/services/getConnection.service.js";
-import ratingRouter from "./src/routes/rating.route.js";
 associationsInitializer();
-getConnectionService().sync({force: true});
+
 const app = express();
 
 app.use("/api", apiRouter);
